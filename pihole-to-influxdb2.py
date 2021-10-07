debf46ef6ca4cdbf76afdecd53895b840c3e36d3
madmpro <32668768+madmpro@users.noreply.github.com>
2021-10-07 20:29:22 +0300

    Update pihole-to-influxdb2.py

--- pihole-to-influxdb2.py
+++ pihole-to-influxdb2.py
@@ -113,11 +113,12 @@ if __name__ == '__main__':
             host = PIHOLE_HOSTS_DICT[index]["host"]
             host_name = PIHOLE_HOSTS_DICT[index]["name"]
             host_port = PIHOLE_HOSTS_DICT[index]["port"]
+            PIHOLE_API = "http://{host}:{host_port}/admin/api.php"
 
             if DEBUG:
                 print(f"\n[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Collecting data for host {host}:{host_port}({host_name})...")
-                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Ping: http://{host}:{host_port}/admin/api.php",file=sys.stderr)
-                ping(PIHOLE_HOSTS_DICT[index]["host"])           
+                print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Ping: {PIHOLE_API}")
+                ping(PIHOLE_HOSTS_DICT)           
            
             try:
                 with urllib.request.urlopen(f"http://{host}:{host_port}/admin/api.php", timeout=10) as url:
